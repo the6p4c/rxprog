dc85ffde451bef5e72b9d9d7c150975cc85bc3e3
The6P4C <watsonjcampbell@gmail.com>
2019-07-05 17:20:27 +1000

    Add programmer struct which connects to device

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
-mod command;
+extern crate serialport;
 
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::time;
+use serialport::prelude::*;
+
+mod command;
+mod programmer;
+
+fn main() -> io::Result<()> {
+    let s = SerialPortSettings {
+        baud_rate: 9600,
+        data_bits: DataBits::Eight,
+        flow_control: FlowControl::None,
+        parity: Parity::None,
+        stop_bits: StopBits::One,
+        timeout: time::Duration::from_millis(100),
+    };
+    let p = serialport::open_with_settings("COM3", &s).expect("Open failed");
+
+    let programmer = programmer::Programmer::new(p);
+    let programmer = programmer.connect()?;
+
+    match programmer {
+        Ok(_) => println!("Connected"),
+        Err(error) => println!("Error: {:?}", error),
+    }
+
+    Ok(())
 }
---
+++ src/programmer.rs
@@ -0,0 +1,60 @@
+use std::io;
+use std::thread;
+use std::time;
+
+use serialport::prelude::*;
+
+#[derive(Debug)]
+pub enum ConnectError {
+    NoResponse,
+    BadResponse,
+    Failed,
+}
+
+pub struct Programmer {
+    p: Box<dyn serialport::SerialPort>,
+}
+
+impl Programmer {
+    pub fn new(p: Box<dyn serialport::SerialPort>) -> Programmer {
+        Programmer { p: p }
+    }
+
+    pub fn connect(mut self) -> io::Result<Result<ProgrammerConnected, ConnectError>> {
+        self.p.clear(serialport::ClearBuffer::All);
+
+        let mut attempts = 0;
+        while self.p.bytes_to_read()? < 1 && attempts < 30 {
+            self.p.write(&[0x00])?;
+            std::thread::sleep(time::Duration::from_millis(10));
+
+            attempts += 1;
+        }
+
+        if attempts >= 30 {
+            return Ok(Err(ConnectError::NoResponse));
+        }
+
+        let mut response1 = [0u8; 1];
+        self.p.read_exact(&mut response1)?;
+        let response1 = response1[0];
+
+        if response1 != 0x00 {
+            return Ok(Err(ConnectError::BadResponse));
+        }
+
+        self.p.write(&[0x55])?;
+
+        let mut response2 = [0u8; 1];
+        self.p.read_exact(&mut response2)?;
+        let response2 = response2[0];
+
+        Ok(match response2 {
+            0xE6 => Ok(ProgrammerConnected(self)),
+            0xFF => Err(ConnectError::Failed),
+            _ => Err(ConnectError::BadResponse),
+        })
+    }
+}
+
+pub struct ProgrammerConnected(Programmer);
